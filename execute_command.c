88d2f3cfed843e7ef700003d6e729b5a0ec0053b
betbaba <bettybettybaba@gmail.com>
2023-11-16 12:32:56 -0800

    new new

--- execute_command.c
+++ execute_command.c
@@ -1,6 +1,5 @@
 #include "custom_shell.h"
 
-
 /**
  * execute_command - Executes a command in the shell
  * @input: Contains the command and arguments in the input string
@@ -8,44 +7,43 @@
  */
 void execute_command(char *input)
 {
-int status;
-pid_t pid;
-int arg_counter;
-char *custom_command;
-char *args[200];
-custom_command = strtok(input, " ");
-args[0] = custom_command;
-arg_counter  = 1;
-while (arg_counter < 199)
-{
-args[arg_counter] = strtok(NULL, " ");
-if (args[arg_counter] == NULL)
-{
-break;
-}
-arg_counter++;
-}
-args[arg_counter] = NULL;
-exit_command(args);
-env_commands(args);
+		int status;
+		pid_t pid;
+		int arg_counter;
+		char *custom_command;
+		char *args[200];
+		custom_command = strtok(input, " ");
+		args[0] = custom_command;
+		arg_counter  = 1;
+		while (arg_counter < 199)
+		{
+				args[arg_counter] = strtok(NULL, " ");
+				if (args[arg_counter] == NULL)
+				{
+						break;
+				}
+				arg_counter++;
+		}
+		args[arg_counter] = NULL;
+		exit_command(args);
+		env_commands(args);
 
+		if (arg_counter > 0)
+		{
+				pid = create_process();
+				if (pid == 0)
+				{
+						search_command_in_path(args);
+				}
+				else if (pid == -1)
+				{
+						perror("fork");
+						exit(1);
+				}
+				else
+				{
+						waitpid(pid, &status, 0);
+				}
+		}
+}
 
-if (arg_counter > 0)
-{
-pid = create_process();
-if (pid == 0)
-{
-search_command_in_path(args);
-}
-else if (pid == -1)
-{
-perror("fork");
-exit(1);
-}
-else
-{
-waitpid(pid, &status, 0);
-}
-}
-
-}
